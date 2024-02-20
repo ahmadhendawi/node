b92ba7b69424891367da5472981168ec6dd4ccc3
ahmadhendawi <ahmad.hendawi200255@gmail.com>
2024-02-20 19:40:47 +0300

    sum

--- nodetues.js
+++ nodetues.js
@@ -1,45 +1,75 @@
 const express= require('express')
 var app=express();
+const {get}= require('http')
 
 
 app.get('/',function(req,res){
 res.send("start server")
 })
 
+app.get('/fact/:num',function(req,res){
+    var factorial=factorial1(req.params.num)
+    res.json("the factorial of this number is " + factorial)
+})
 
-function factorial1(){
+    app.get('/multiply/:num',function(req,res){
+    var multiply=multiplication((req.params.num))
+    res.json("the multiplication table of this number is " + multiply)})
+    
+    app.get('/fib/:num',function(req,res){
+        var fib=fibonacci(req.params.num)
+        res.json("the fibonacci sequence of this number is " + fibo)})
+
+    app.get('/comm/:num',function(req,res) {
+        var comm =commulative(req.params.num)
+        res.json("the commulative of this number is " + comm)})
+        
+    app.get('/word/:string',function(req,res) {
+ 
+            const word =req.params.string
+            const result =stringa(word)
+            res.json( result)})
+        
+    var server= app.listen(9000, function(){
+    var host = server.address().address
+    var port = server.address().port
+})
+function factorial1(num){
     let fact = 1;
-    for (i = 1; i <= 10; i++) {
-        fact= num*i;
+    for (let i = 1; i <= num ; i++) {
+        fact= fact*i;
         
     }
     return fact
 }
-function multiply(){
+function multiplication(x){
+    let f=x
+    let z=[]
     for(let i = 1; i <= 10; i++) {
-        const result = i * num;
-    
+        z.push(f*i)
     }
-    return result
+    return z
 }
-function fib(){
-    console.log("")
+function fibonacci(z){
+    let x=0
+    const seq=[1,1]
+    for (let i=2 ; i<=z ; i++){
+    a=(i-1)+(i-2)}
 }
-app.get('/fact/:num',function(req,res){
-    console.log(req.params.num)
-    var factorial=factorial1(req.params.num)
-    res.json("the factorial of this number is" + factorial)
-})
+function commulative(z){
+    let x=0
+    for( let i=0;i<=z;i++)
+    {
+    x=x+i}
+    return x
+}
+function stringa(word){
 
-    app.get('/multiply/:num',function(req,res){
-    var multiply=multiply1(req.params.num)
-    res.json("the multiplication table of this number is" + multiply )})
-    
-    app.get('/fib/:num',function(req,res){
-        var fib=fib1(req.params.num)
-        res.json("the factorial of this number is" + fib)})
-
-        var server= app.listen(9000, function(){
-    var host = server.address().address
-    var port = server.address().port
-})
+    const len =word.length
+    for(let i=0;i<len;i++)
+    {
+    if (word[i] !== word[len-1-i]){
+    return false
+        }}
+    return true
+}
